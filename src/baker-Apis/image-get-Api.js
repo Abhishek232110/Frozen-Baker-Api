064a497dce15b79ede081bf7820431afb286d4a0
Abhishek232110 <abhi7376862392@gmail.com>
2024-02-10 02:06:54 +0530

    filter api

--- src/baker-Apis/image-get-Api.js
+++ src/baker-Apis/image-get-Api.js
@@ -20,8 +20,35 @@ uploadImage.get("/api/get/butterscotch", async (req, res) => {
   res.send(data);
 });
 
-uploadImage.get("/get", async (req, res) => {
-  const query = { flavour: "Chocolate" };
+uploadImage.get("/api/get/flower", async (req, res) => {
+  const query = { flavour: "Flower" };
+  const data = await cakeSchema.find(query);
+  res.send(data);
+});
+
+uploadImage.get("/api/get/truffle", async (req, res) => {
+  const query = { flavour: "Truffle" };
+  const data = await cakeSchema.find(query);
+  res.send(data);
+});
+
+uploadImage.get("/api/get/redvelvet", async (req, res) => {
+  const query = { flavour: "Redvelvet" };
+  const data = await cakeSchema.find(query);
+  res.send(data);
+});
+uploadImage.get("/api/get/vanilla", async (req, res) => {
+  const query = { flavour: "Vanilla" };
+  const data = await cakeSchema.find(query);
+  res.send(data);
+});
+uploadImage.get("/api/get/fruit", async (req, res) => {
+  const query = { flavour: "Fruit" };
+  const data = await cakeSchema.find(query);
+  res.send(data);
+});
+uploadImage.get("/api/get/pineapple", async (req, res) => {
+  const query = { flavour: "Pineapple" };
   const data = await cakeSchema.find(query);
   res.send(data);
 });
